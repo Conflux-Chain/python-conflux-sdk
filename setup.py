11df25b78418da6fa3607a08d33d53633f3884b7
Zhang Wenda <wenda.zhang@confluxnetwork.org>
2022-09-02 17:25:31 +0800

    Bump version: 0.2.0-beta.1 → 1.0.0-beta.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import (
     setup,
 )
 
-VERSION = "0.2.0-beta.1"
+VERSION = "1.0.0-beta.1"
 DESCRIPTION = 'Python SDK for Conflux network'
 with open('./README.md') as readme:
     long_description = readme.read()
