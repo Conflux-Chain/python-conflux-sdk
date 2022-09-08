e67b4a17af2b8cf22259245e0998720d81b996ee
Zhang Wenda <wenda.zhang@confluxnetwork.org>
2022-09-08 17:11:35 +0800

    Fix wrong manifest usage

--- tests/pm/test_pm.py
+++ tests/pm/test_pm.py
@@ -2,11 +2,11 @@ from ethpm import Package, ASSETS_DIR
 
 def test_contract_factory(w3):
     # ethpm_spec_dir = get_ethpm_spec_dir()
-    erc20_manifest_path = ASSETS_DIR / 'standard-token' / 'with_bytecode_v3.json'
+    escrow_manifest_path = ASSETS_DIR / 'escrow' / 'with_bytecode_v3.json'
     # erc20_manifest_path = '/Users/conflux-y/Documents/code/web3.py/ethpm/assets/standard-token/with_bytecode_v3.json'
-    ERC20Package = Package.from_file(erc20_manifest_path, w3)
-    assert isinstance(ERC20Package, Package)
+    escrow_package = Package.from_file(escrow_manifest_path, w3)
+    assert isinstance(escrow_package, Package)
     
-    factory = ERC20Package.get_contract_factory("StandardToken") # type: ignore
+    factory = escrow_package.get_contract_factory("Escrow") # type: ignore
     
     assert factory
