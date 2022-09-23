232265056df90ff997b653bef890d4d5f302b3fd
Zhang Wenda <wenda.zhang@confluxnetwork.org>
2022-09-23 16:37:33 +0800

    Only estimate once when fill transaction defaults

--- conflux_web3/_utils/transactions.py
+++ conflux_web3/_utils/transactions.py
@@ -20,18 +20,17 @@ from conflux_web3.types import TxParam
 if TYPE_CHECKING:
     from conflux_web3 import Web3
 
-# TODO: don't estimate twice for gas and storageLimit
+
 TRANSACTION_DEFAULTS = {
     "value": 0,
     "data": b"",
-    "nonce": lambda w3, tx: w3.cfx.get_next_nonce(tx['from']),
-    "gas": lambda w3, tx: w3.cfx.estimate_gas_and_collateral(tx)["gasLimit"],
-    "storageLimit": lambda w3, tx: w3.cfx.estimate_gas_and_collateral(tx)["storageCollateralized"],
-    "gasPrice": lambda w3, tx: w3.cfx.gas_price,
-    "chainId": lambda w3, tx: w3.cfx.chain_id,
-    "epochHeight": lambda w3, tx: w3.cfx.epoch_number,
+    "nonce": lambda w3, tx, estimate=None: w3.cfx.get_next_nonce(tx['from']),
+    "gas": lambda w3, tx, estimate=None: estimate["gasLimit"],
+    "storageLimit": lambda w3, tx, estimate=None: estimate["storageCollateralized"],
+    "gasPrice": lambda w3, tx, estimate=None: w3.cfx.gas_price,
+    "chainId": lambda w3, tx, estimate=None: w3.cfx.chain_id,
+    "epochHeight": lambda w3, tx, estimate=None: w3.cfx.epoch_number,
 }
-
 
 
 @curry
@@ -46,9 +45,14 @@ def fill_transaction_defaults(w3: "Web3", transaction: TxParam) -> TxParam:
         raise ValueError("Transaction's 'from' field is required to fill nonce field")
     
     for key, default_getter in TRANSACTION_DEFAULTS.items():
+        estimate = None
         if key not in transaction:
             if callable(default_getter):
-                default_val = default_getter(w3, transaction)
+                if not estimate:
+                    if key == "gas" or key == "storageLimit":
+                        estimate = w3.cfx.estimate_gas_and_collateral(transaction)
+                default_val = default_getter(w3, transaction, estimate)
+                
             else:
                 default_val = default_getter
 
