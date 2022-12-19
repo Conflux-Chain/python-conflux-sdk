7b4330466e787103575fea07758d02c63cb535e5
Darwin <wenda.zhang@confluxnetwork.org>
2022-12-19 18:12:10 +0800

    Bump version: 1.0.0-beta.8 → 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import (
     setup,
 )
 
-VERSION = "1.0.0-beta.8"
+VERSION = "1.0.0"
 DESCRIPTION = 'Python SDK for Conflux network'
 with open('./README.md') as readme:
     long_description = readme.read()
