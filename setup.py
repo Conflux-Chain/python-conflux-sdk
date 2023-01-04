88216c472be020e0ffa565deb50c733547057d0c
Darwin <wenda.zhang@confluxnetwork.org>
2023-01-04 14:58:13 +0800

    doc: add missing support 3.7 version in setup.py

--- setup.py
+++ setup.py
@@ -104,6 +104,7 @@ setup(
     keywords=['python', 'conflux', 'blockchain'],
     classifiers=[
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
