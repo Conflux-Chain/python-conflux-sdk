e6e08eea5f8d8fb4a485d760b84e245bf48f8f2d
darwintree <17946284+darwintree@users.noreply.github.com>
2024-11-04 10:51:19 +0800

    chore: web3py7 middleware migration

--- setup.py
+++ setup.py
@@ -96,9 +96,9 @@ setup(
                   "cns": ["py.typed"]},
     url='https://github.com/conflux-chain/python-conflux-sdk',
     install_requires=[
-        "web3==6.20.3",
+        "web3==7.4.0",
         "cfx-address>=1.2.0",
-        "cfx-account>=1.1.0",
+        "cfx-account>=1.2.0",
         "cfx-utils>=1.0.5",
     ],  # add any additional packages that
     # needs to be installed along with your package. Eg: 'caer'
