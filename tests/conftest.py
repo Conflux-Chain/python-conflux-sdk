274151f8a028ef2eab0793e9980d606616f0851d
darwintree <17946284+darwintree@users.noreply.github.com>
2024-12-16 14:30:21 +0800

    tests: improve ci secret setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,33 +22,50 @@ def use_testnet() -> bool:
     return bool(os.environ.get("TESTNET_SECRET")) or bool(os.environ.get("USE_TESTNET"))
 
 @pytest.fixture(scope="session")
-def node(use_testnet: bool, worker_id: str) -> Iterable[BaseNode]:
+def worker_index(worker_id: str) -> int:
+    if worker_id == "master":
+        return 0
+    return int(worker_id[2:])
+
+@pytest.fixture(scope="session")
+def node(use_testnet: bool, worker_index: int) -> Iterable[BaseNode]:
     if use_testnet:
         node = RemoteTestnetNode() # connection error might occur if using public RPC
         # node = LocalTestnetNode() 
         yield node
         # node.exit()
     else:
-        num = 0 if worker_id == "master" else int(worker_id[2:])
-        node = LocalNode(node_name=f"sdk-test-{num}", index=num)
+        node = LocalNode(node_name=f"sdk-test-{worker_index}", index=worker_index)
         yield node
         # node.exit()
 
 @pytest.fixture(scope="session")
-def node_url(node: Iterable[LocalNode]) -> str:
+def node_url(node: BaseNode) -> str:
     return node.url
 
 @pytest.fixture(scope="session")
-def secret_key(node: LocalNode) -> Union[str, None]:
+def secret_key(node: LocalNode, worker_index: int, use_testnet: bool) -> Union[str, None]:
     """
     Returns:
         str: secret key with enough balance
     """
-    return node.secrets[0]
+    try:
+        return node.secrets[worker_index]
+    except IndexError as e:
+        if not use_testnet:  # faucet_account is enabled
+            raise e
+        w3 = Web3(Web3.HTTPProvider(node.url))
+        acct = w3.cfx.account.create()
+        w3.wallet.add_account(acct)
+        faucet = w3.cfx.contract(name="Faucet")
+        faucet.functions.claimCfx().transact({
+            "from": acct.address,
+        }).executed()
+        return f"0x{acct.key.hex()}"
 
 # no scope here
 @pytest.fixture
-def w3(node_url: str, node: LocalNode) -> Web3:
+def w3(node_url: str) -> Web3:
     """
     Returns:
         Web3: a web3 instance
@@ -64,34 +81,25 @@ def account(node_url: str, secret_key: str) -> LocalAccount:
     provider = Web3.HTTPProvider(node_url)
     w3 = Web3(provider=provider)
     acct = w3.account.from_key(secret_key)
-    if w3.cfx.get_balance(acct.address) != 0:
-        return w3.account.from_key(secret_key)
-    elif w3.cfx.chain_id == 1:
-        w3.cfx.default_account = acct
-        faucet = w3.cfx.contract(name="Faucet")
-        faucet.functions.claimCfx().transact().executed()
-        return acct
-    else:
-        raise Exception("Unexpected exception: not local nor testnet node and no environment secret key is set")
+    if w3.cfx.get_balance(acct.address) == 0:
+        raise Exception("Unexpected exception: provided account has no balance")
+    return w3.account.from_key(secret_key)
 
 @pytest.fixture(scope="module")
-def moduled_w3(node_url: str, node: LocalNode, account) -> Web3:
+def moduled_w3(node_url: str, account: LocalAccount) -> Web3:
     """
     a web3 instance for the convenience to create module shared objects
     e.g. a transaction or a contract which is required for the module
     NOTE: DON'T CHANGE PROPERTY OF THIS W3
     """
-    # account = Account.from_key(secret_key, )
     provider = Web3.HTTPProvider(node_url)
     w3 = Web3(provider=provider)
     w3.cfx.default_account = account
     return w3
 
 @pytest.fixture(scope="session")
-def address(node_url, secret_key) -> str:
-    chain_id = Web3(Web3.HTTPProvider(node_url)).cfx.chain_id
-    addr = Account.from_key(secret_key, chain_id).address
-    return addr
+def address(account: LocalAccount) -> Base32Address:
+    return account.base32_address
 
 @pytest.fixture
 def embedded_accounts(w3: Web3, use_testnet: bool) -> Sequence[Base32Address]:
