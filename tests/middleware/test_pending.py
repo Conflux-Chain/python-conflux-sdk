61a78d246000d02e63afff8888d1c95616cf6881
darwintree <17946284+darwintree@users.noreply.github.com>
2024-12-16 14:29:47 +0800

    tests: use pytest-order to test pending first

--- tests/middleware/test_pending.py
+++ tests/middleware/test_pending.py
@@ -7,30 +7,19 @@ from cfx_account.account import LocalAccount
 if TYPE_CHECKING:
     from conflux_web3 import Web3
 
+@pytest.mark.order(1)
 @pytest.mark.xdist_group(name="pending")   
 def test_pending(w3: "Web3", account: LocalAccount, use_testnet: bool):
     # activate by default
     # w3.middleware_onion.add(PendingTransactionMiddleware)
-    
-    status = w3.cfx.get_status()
     addr = account.address
+    w3.wallet.add_account(account)
     
-    tx = {
-        'from': addr,
-        'nonce': w3.cfx.get_next_nonce(addr),
-        'gas': 21000,
-        'to': w3.cfx.account.create().address,
-        'value': 100,
-        'maxFeePerGas': 2 * w3.cfx.gas_price,
-        'maxPriorityFeePerGas': 0,
-        'chainId': w3.cfx.chain_id,
-        'storageLimit': 0,
-        'epochHeight': status['epochNumber']
-    }
-    signed = account.sign_transaction(tx)
-    rawTx = signed.raw_transaction
-    pending = w3.cfx.send_raw_transaction(rawTx)
-    # hash = cast(PendingTransaction, hash)
+    pending = w3.cfx.send_transaction({
+        "from": addr, 
+        "to": w3.cfx.account.create().address, 
+        "value": 100,
+    })
     pending.mined()
     pending.executed()
     pending.confirmed()
