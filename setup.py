f0fd3f6aae389bc21f3fff1b0ad4c575090aa990
darwintree <17946284+darwintree@users.noreply.github.com>
2025-03-31 10:39:35 +0800

    Bump version: 1.4.1 → 1.4.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import (
     setup,
 )
 
-VERSION = "1.4.1"
+VERSION = "1.4.2"
 DESCRIPTION = 'Python SDK for Conflux network'
 with open('./README.md') as readme:
     long_description = readme.read()
