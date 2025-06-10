c5fd650e3c0d1484acad5e58371ee01b420f31e6
darwintree <17946284+darwintree@users.noreply.github.com>
2025-06-10 11:12:16 +0800

    Bump version: 1.4.3 → 1.4.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import (
     setup,
 )
 
-VERSION = "1.4.3"
+VERSION = "1.4.4"
 DESCRIPTION = 'Python SDK for Conflux network'
 with open('./README.md') as readme:
     long_description = readme.read()
--- tests/base_features/test_api.py
+++ tests/base_features/test_api.py
@@ -1,4 +1,4 @@
 def test_api_version(w3):
-    expected_version = "1.4.3"
+    expected_version = "1.4.4"
     if "beta" not in expected_version:
         assert w3.api.startswith(expected_version)
